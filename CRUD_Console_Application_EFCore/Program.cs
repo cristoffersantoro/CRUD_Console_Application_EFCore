75bc7a8eb2c5b3423e23fa74fe6f625bb120932f
cristoffersantoro <cristoffersantoro@gmail.com>
2020-08-01 16:31:04 -0300

    first commit

---
+++ CRUD_Console_Application_EFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Console_Application_EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
